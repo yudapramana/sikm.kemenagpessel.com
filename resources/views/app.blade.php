d7a10bcaa368adf54e99e33b75928fdc1c050914
Pramana Yuda Sayeti <pramanayuda772@gmail.com>
2023-03-21 11:23:31 +0700

    push

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         }
     </style>
 
-    <title>SIMBUTU DIGITAL - Sistem Informasi Buku Tamu Digital</title>
+    <title>SIKM KemenagPessel - Sistem Indeks Kepuasan Masyarakat</title>
 
     <script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>
 </head>
