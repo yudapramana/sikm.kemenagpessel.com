9df44a2e879dd93177a137dde285960d3f32f35d
pramana yuda <pramanayuda772@gmail.com>
2023-06-22 14:20:11 +0700

    push

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             position: fixed !important;
             bottom: 0 !important;
         }
+
     </style>
 
     <title>SIKM KemenagPessel - Sistem Indeks Kepuasan Masyarakat</title>
@@ -33,6 +34,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>
 
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script defer>
+        document.addEventListener('load', function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://widget.equally.ai/equally-widget.min.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'equallyWidget'));
+        !window.EQUALLY_AI_API_KEY && (window.EQUALLY_AI_API_KEY = "Pd3zEXpVDjkEOuUWmk7R", intervalId = setInterval(function() {
+            window.EquallyAi && (clearInterval(intervalId), window.EquallyAi = new EquallyAi)
+        }, 500));
+
+    </script>
 
 </body>
 
